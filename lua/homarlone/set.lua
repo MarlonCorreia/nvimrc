da1a70e6fb5136f7314abf64b606168027fb2b0d
marloncorreia <malilocsimas26@gmail.com>
2023-12-23 15:32:32 -0300

    feat: Config neotree

--- lua/homarlone/set.lua
+++ lua/homarlone/set.lua
@@ -11,5 +11,8 @@ vim.cmd.colorscheme("onedark")
 
 vim.api.nvim_set_hl(0, "@punctuation.bracket", { link = "" })
 
-
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
