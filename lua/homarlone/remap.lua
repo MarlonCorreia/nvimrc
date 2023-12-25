e6a1caae8dc8fb47654c84e595dfd56c0d836a9e
marloncorreia <malilocsimas26@gmail.com>
2023-12-25 01:08:06 -0300

    feat: Mov sets and remaps

--- lua/homarlone/remap.lua
+++ lua/homarlone/remap.lua
@@ -1,3 +1,7 @@
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-
-
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- lua/homarlone/set.lua
+++ lua/homarlone/set.lua
@@ -16,3 +16,6 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.hlsearch = false 
