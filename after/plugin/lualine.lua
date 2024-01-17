6f6a5221a0cf4c623c2067d25ba01242282770db
MarlonCorreia <malilocsimas26@gmail.com>
2024-01-16 22:00:58 -0300

    feat: Show only parent folder of filename

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -11,7 +11,26 @@ require("lualine").setup({
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diagnostics'},
-        lualine_c = {{'filename', path=1 }, 'diff'},
+        lualine_c = {
+            {
+
+                function()
+                    local path = vim.fn.expand('%:p:h')
+                    local folders = vim.fn.split(path, '/')
+                    return folders[#folders]
+                end,
+                icon = '',
+                separator = '/',
+                padding = {left=1, right=0},
+
+            },
+            {
+                'filename',
+                padding = {left=0, right=1}
+
+            },
+            {'diff'}
+        },
         lualine_x = {},
         lualine_y = {'progress'},
         lualine_z = {'location'}
